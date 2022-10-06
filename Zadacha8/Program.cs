89f429f385576bd9c47f5dfe0c57ebb67b268dac
masyaka.94 <masyaka.94@gmail.com>
2022-10-06 21:26:16 +0500

    dobavlena zadacha 8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Vvedite chislo N");
+int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+while (i < N) 
+{
+    if (i % 2 == 0) 
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
