bf4ef6eed8a0426653868ed65b0beced016a829d
masyaka.94 <masyaka.94@gmail.com>
2022-10-06 21:13:00 +0500

    korrektirovka zadachi 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine ("Vvedite chislo a");
+int a = int.Parse(Console.ReadLine());
+
+if (a % == 0)
+{
+Console.WriteLine (а + " - chetnoe chislo");
+}
+else
+{
+Console.WriteLine (а + " - nechetnoe chislo");
+}
