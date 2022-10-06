cef870ed5dce1cd5ded33aa84997d51ba8f528ee
masyaka.94 <masyaka.94@gmail.com>
2022-10-06 20:40:50 +0500

    исправлены ошибки в задаче 2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какой меньшее.
 
 Console.WriteLine ("Vvedite chislo a");
-int a=int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 Console.WriteLine ("Vvedite chislo b");
-int b=Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
 int max = 0;
 int min = 0;
