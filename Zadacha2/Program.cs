040224e99889ed44b7b8f31fbcfe3c7e2c0aa6f3
masyaka.94 <masyaka.94@gmail.com>
2022-10-06 20:38:13 +0500

    добавлена задача 2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какой меньшее.
+
+Console.WriteLine ("Vvedite chislo a");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine ("Vvedite chislo b");
+int b=Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (a>b) 
+{
+max = a;
+min = b;
+Console.WriteLine ("maximal'noe chislo " + max);
+Console.WriteLine ("minimal'noe chislo " + min);
+}
+else
+{
+max = b;
+min = a;
+Console.WriteLine ("maximal'noe chislo " + max);
+Console.WriteLine ("minimal'noe chislo " + min);
+}
